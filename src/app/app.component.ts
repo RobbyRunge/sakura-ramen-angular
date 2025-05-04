5b4240b6baad69e451aca514b204c96ba89ec6b7
RobbyR98 <robbyrunge@yahoo.com>
2025-05-04 16:34:00 +0200

    Remove ImprintComponent from app imports to streamline the application structure.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,10 @@ import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MainContentComponent } from "./main-content/main-content.component";
 import { FooterComponent } from './shared/components/footer/footer.component';
-import { ImprintComponent } from './imprint/imprint.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MainContentComponent, FooterComponent, ImprintComponent],
+  imports: [CommonModule, RouterOutlet, MainContentComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
