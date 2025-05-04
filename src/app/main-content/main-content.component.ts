4cb569c369304432b89304103f4162e3f1faf8d1
RobbyR98 <robbyrunge@yahoo.com>
2025-05-04 16:25:47 +0200

    Add FindUs component and integrate it into the main content structure; update main-content template and styles accordingly.

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -3,11 +3,12 @@ import { CommonModule } from '@angular/common';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { OurMenuComponent } from './our-menu/our-menu.component';
 import { HowToOrderComponent } from "./how-to-order/how-to-order.component";
+import { FindUsComponent } from './find-us/find-us.component';
 
 @Component({
   selector: 'app-main-content',
   standalone: true,
-  imports: [CommonModule, LandingPageComponent, OurMenuComponent, HowToOrderComponent],
+  imports: [CommonModule, LandingPageComponent, OurMenuComponent, HowToOrderComponent, FindUsComponent],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.scss'
 })
